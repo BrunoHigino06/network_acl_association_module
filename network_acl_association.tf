09a3ab74cad9db3857dcb4b836685ac7bd3a0bbe
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-07-05 20:09:59 -0300

    2022.07.05

--- network_acl_association.tf
+++ network_acl_association.tf
@@ -1,5 +1,5 @@
 resource "aws_network_acl_association" "network_acl" {
     count = length(var.subnet_name_association)
-    network_acl_id = data.aws_network_acls.networkAcl_name[count.index].id
+    network_acl_id = data.aws_network_acls.networkAcl_name.id
     subnet_id      = data.aws_subnet.subnet_name[count.index].id
 }
